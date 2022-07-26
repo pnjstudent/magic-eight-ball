439c37e6c4c36cbb004f6cbb7c2430cb58860d9b
pnjstudent <110066984+pnjstudent@users.noreply.github.com>
2022-07-26 18:24:08 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("" + (list._pickRandom()))
+})
+let list: string[] = []
+list = [
+"It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful"
+]
